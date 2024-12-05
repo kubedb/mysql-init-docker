c5603b799dd76334750157a40614fba1b58920e6
SK Ali Arman <arman.cse4.bu@gmail.com>
2024-12-05 16:24:58 +0600

    Add PITR Replication Strategy Support & Fix Same Memeber_ID (#42)
    Signed-off-by: SK Ali Arman <arman@appscode.com>

--- init-script/run.sh
+++ init-script/run.sh
@@ -2,4 +2,5 @@
 
 rm -rf /var/lib/mysql/lost+found
 rm -rf /run-scripts/*
+rm /var/lib/mysql/auto.cnf
 cp /tmp/scripts/* /scripts
--- scripts/run.sh
+++ scripts/run.sh
@@ -17,6 +17,21 @@
 #   POD_IP_TYPE         = Address type of POD_IP (one of IPV4, IPv6)
 
 env | sort | grep "POD\|HOST\|NAME"
+RECOVERY_DONE_FILE="/tmp/recovery.done"
+if [[ "$PITR_RESTORE" == "true" ]]; then
+    while true; do
+      sleep 2
+      echo "Point In Time Recovery In Progress. Waiting for $RECOVERY_DONE_FILE file"
+      if [[ -e "$RECOVERY_DONE_FILE" ]]; then
+        echo "$RECOVERY_DONE_FILE found."
+        break
+      fi
+    done
+fi
+
+if [[ -e "$RECOVERY_DONE_FILE" ]]; then
+  rm $RECOVERY_DONE_FILE
+fi
 
 if [ "$POD_IP_TYPE" = "IPv6" ]; then
     log "ERROR" "MySQL 5.7 is not supported on an IPv6 cluster."
