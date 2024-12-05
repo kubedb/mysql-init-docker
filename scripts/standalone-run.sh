7a670b553b7fa956a9d80a5bc8e11e1202f0d7d2
SK Ali Arman <arman.cse4.bu@gmail.com>
2024-12-05 16:25:12 +0600

    Add PITR Replication Strategy Support & Fix Same Memeber_ID (#43)
    Signed-off-by: SK Ali Arman <arman@appscode.com>

---
+++ scripts/standalone-run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [[ -z "$TOPOLOGY" ]]; then
+  RECOVERY_DONE_FILE="/tmp/recovery.done"
+  if [[ "$PITR_RESTORE" == "true" ]]; then
+    while true; do
+      sleep 2
+      echo "Point In Time Recovery In Progress. Waiting for $RECOVERY_DONE_FILE file"
+      if [[ -e "$RECOVERY_DONE_FILE" ]]; then
+        echo "$RECOVERY_DONE_FILE found."
+        break
+      fi
+    done
+  fi
+
+  if [[ -e "$RECOVERY_DONE_FILE" ]]; then
+    rm $RECOVERY_DONE_FILE
+  fi
+fi
