4b6bb2e49a974e298e1b05549d1dec73de9e2237
SK Ali Arman <arman.cse4.bu@gmail.com>
2024-12-05 16:25:12 +0600

    Add PITR Replication Strategy Support & Fix Same Memeber_ID (#43)
    Signed-off-by: SK Ali Arman <arman@appscode.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -16,6 +16,21 @@
 #   PRIMARY_TYPE        = defines single/multi primary
 
 env | sort | grep "POD\|HOST\|NAME"
+RECOVERY_DONE_FILE="/tmp/recovery.done"
+if [[ "$PITR_RESTORE" == "true" ]]; then
+    while true; do
+      sleep 2
+      echo "Point In Time Recovery In Progress. Waiting for $RECOVERY_DONE_FILE file"
+      if [[ -e "$RECOVERY_DONE_FILE" ]]; then
+        echo "$RECOVERY_DONE_FILE found."
+        break
+      fi
+    done
+fi
+
+if [[ -e "$RECOVERY_DONE_FILE" ]]; then
+  rm $RECOVERY_DONE_FILE
+fi
 
 args=$@
 script_name=${0##*/}
