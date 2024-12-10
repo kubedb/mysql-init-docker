0bf6660cd95555a50d8570602a23582ff4ca2cc7
SK Ali Arman <arman@appscode.com>
2024-12-10 10:52:45 +0600

    Update Member ID Removal
    Signed-off-by: SK Ali Arman <arman@appscode.com>

--- init-script/run.sh
+++ init-script/run.sh
@@ -2,5 +2,10 @@
 
 rm -rf /var/lib/mysql/lost+found
 rm -rf /run-scripts/*
-rm /var/lib/mysql/auto.cnf
 cp /tmp/scripts/* /scripts
+if [[ "$PITR_RESTORE" == "true" ]]; then
+  if [[ "$HOSTNAME" != *"-0" ]]; then
+    rm /var/lib/mysql/auto.cnf
+  fi
+fi
+
