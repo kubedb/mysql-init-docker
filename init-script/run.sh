e464113b6d7aa54079de92ce59bf850407ae909e
Tamal Saha <tamal@appscode.com>
2025-02-02 22:43:45 +0600

    Enable license restriction and client billing
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- init-script/run.sh
+++ init-script/run.sh
@@ -4,9 +4,9 @@ rm -rf /var/lib/mysql/lost+found
 rm -rf /run-scripts/*
 cp /tmp/scripts/* /scripts
 if [[ "$PITR_RESTORE" == "true" ]]; then
-  if [[ "$HOSTNAME" != *"-0" ]]; then
-    if [[ -f /var/lib/mysql/auto.cnf ]]; then
-       rm /var/lib/mysql/auto.cnf
+    if [[ "$HOSTNAME" != *"-0" ]]; then
+        if [[ -f /var/lib/mysql/auto.cnf ]]; then
+            rm /var/lib/mysql/auto.cnf
+        fi
     fi
-  fi
 fi
--- scripts/standalone-run.sh
+++ scripts/standalone-run.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 if [[ -z "$TOPOLOGY" ]]; then
-  RECOVERY_DONE_FILE="/tmp/recovery.done"
-  if [[ "$PITR_RESTORE" == "true" ]]; then
-    while true; do
-      sleep 2
-      echo "Point In Time Recovery In Progress. Waiting for $RECOVERY_DONE_FILE file"
-      if [[ -e "$RECOVERY_DONE_FILE" ]]; then
-        echo "$RECOVERY_DONE_FILE found."
-        break
-      fi
-    done
-  fi
+    RECOVERY_DONE_FILE="/tmp/recovery.done"
+    if [[ "$PITR_RESTORE" == "true" ]]; then
+        while true; do
+            sleep 2
+            echo "Point In Time Recovery In Progress. Waiting for $RECOVERY_DONE_FILE file"
+            if [[ -e "$RECOVERY_DONE_FILE" ]]; then
+                echo "$RECOVERY_DONE_FILE found."
+                break
+            fi
+        done
+    fi
 
-  if [[ -e "$RECOVERY_DONE_FILE" ]]; then
-    rm $RECOVERY_DONE_FILE
-  fi
+    if [[ -e "$RECOVERY_DONE_FILE" ]]; then
+        rm $RECOVERY_DONE_FILE
+    fi
 fi
